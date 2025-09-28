5fc06fca397dff634651f9b48ef1d72a41b04c5c
ziyang Ou <jonhnny168@gmail.com>
2025-09-28 14:16:21 -0400

    initial

---
+++ assignment1.py
@@ -0,0 +1,10 @@
+# Name this file to assignment1.py when you submit
+
+# The pathfinding function must implement A* search to find the goal state
+def pathfinding(filepath):
+  # filepath is the path to a CSV file containing a grid 
+
+  # optimal_path is a list of coordinate of squares visited (in order)
+  # optimal_path_cost is the cost of the optimal path
+  # num_states_explored is the number of states explored during A* search
+  return optimal_path, optimal_path_cost, num_states_explored
