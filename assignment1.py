c0aa3ff08599ce638e39b81f3c2f829e6eed71da
BaguetteStick <93622408+BaguetteStick@users.noreply.github.com>
2025-09-28 15:58:46 -0400

    calculate heuristics function, calculate manhatten distance function, calculate successors function
    Added Manhattan distance function and heuristic for pathfinding.

--- assignment1.py
+++ assignment1.py
@@ -8,3 +8,33 @@ def pathfinding(filepath):
   # optimal_path_cost is the cost of the optimal path
   # num_states_explored is the number of states explored during A* search
   return optimal_path, optimal_path_cost, num_states_explored
+
+
+def manhatten(start_coord, end_coord):
+    mahatten_dist = [start_coord[0] - end_coord[0], start_coord[1] - end_coord[1]]
+    return mahatten_dist
+
+
+def heuristic(curr_coord, treasure_total, treasure_coords, goal_coords):
+    if treasure_total >= 5:
+        dist_goal = []
+        for goal in goal_coords:
+            dist_goal.append(manhatten(curr_coord, goal))
+        return min(dist_goal)
+    
+    dist = []
+    for treasure in treasure_coords:
+        for goal in goal_coords:
+            dist.append(manhatten(curr_coord, treasure) + manhatten(treasure, goal))
+
+    return min(dist)
+
+
+def successors(curr_coord):
+    directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+    children = []
+    for direction in directions:
+        new_coord = (curr_coord[0] + direction[0], curr_coord[1] + direction[1])
+        if new_coord not in walls and 0 <= new_coord[0] < grid_size[0] and 0 <= new_coord[1] < grid_size[1]:
+            children.append(new_coord)
+    return children
